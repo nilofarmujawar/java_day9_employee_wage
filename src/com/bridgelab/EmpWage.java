4ee7ab8d21fdb78e743dd014dfc8b05f4d27f243
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-22 16:38:50 +0530

    Added: Program for calculate daily employee wage.

--- src/com/bridgelab/EmpWage.java
+++ src/com/bridgelab/EmpWage.java
@@ -2,34 +2,47 @@ package com.bridgelab;
 
 import java.util.Random;
 
-//uc1
+//uc2
 
 /*
-check employee is present or absent
+calculate daily employee wage.
  */
 
-class utilityEmpWage{
 
+
+class utilityEmpWage {
     Random rand = new Random();
     public int isPresent = 1;
     public int isAbsent = 2;
+    public int wagePerHr = 20;
+    public int fullDayHr = 8;
+    int check = rand.nextInt(2) + 1;
 
-    public void present() {
-        int check = rand.nextInt(2)+1;
+    public boolean present() {
         if (check == isAbsent) {
             System.out.println("Employee is absent.");
-        }
-        else {
+            return false;
+        } else {
             System.out.println("Employee is present.");
+            return true;
+        }
+    }
+
+    public void dailyWage() {
+        if (present()) {
+            int dailyWage = wagePerHr * fullDayHr;
+            System.out.println("Wage of the day of employee is : " + dailyWage);
         }
     }
 }
+
 public class EmpWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
-        utilityEmpWage empWage = new utilityEmpWage();  //create an a empWage object for utilityEmpWage class
+        utilityEmpWage empWage = new utilityEmpWage();  //create an empWage object for utilityEmpWage class
         empWage.present();
+        empWage.dailyWage();
     }
 
 }
