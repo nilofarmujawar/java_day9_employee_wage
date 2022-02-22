112d1a5e09863589d59f74dc72878269832f6d27
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-22 17:01:42 +0530

    Added: Program for calculate the total working hr and day is reache….

--- src/com/bridgelab/EmpWage.java
+++ src/com/bridgelab/EmpWage.java
@@ -2,12 +2,12 @@ package com.bridgelab;
 
 import java.util.Random;
 
-//uc5
+//uc6
 
 /*
-      calculating wages for a month
- */
+      Calculate Wages till a condition of total working hours or days is reached for a month
 
+ */
 
 class utilityEmpWage {
     Random rand = new Random();
@@ -17,26 +17,41 @@ class utilityEmpWage {
     public int wagePerHr = 20;
     public int fullDayHr = 8;
     public int partTimeHr = 4;
+    public int maxWorkingDays = 20;
+    public int workingDays = 0;
+    public int maxWorkingHr = 100;
+    public int workingHr = 0;
     public int dailyWage;
+    public int hrs = 0;
 
     public void present() {
-        int check = rand.nextInt(3) + 1;
-        switch (check) {
+        /*
+        using while loop
+         */
+        while (workingHr < maxWorkingHr && workingDays < maxWorkingDays) {
+            int check = rand.nextInt(3) + 1;
 
-            case isPresent : {
-                System.out.println("Employee is present.");
-                dailyWage = wagePerHr * fullDayHr;
-                System.out.println("Daily wage is : " + dailyWage);
+            /*
+            using switch case for
+             */
+
+            switch (check) {
+
+                case isPresent : {
+                    System.out.println("Employee is present.");   //condition1
+                    hrs = fullDayHr;
+                }
+                case isPartTime : {
+                    System.out.println("Employee is present part time.");   //condition2
+                    hrs = partTimeHr;
+                }
+                case isAbsent : hrs = 0;
             }
-
-            case isPartTime : {
-                System.out.println("Employee is present part time.");
-                dailyWage = wagePerHr * partTimeHr;
-                System.out.println("Daily wage is : " + dailyWage);
-            }
-
-            case isAbsent : System.out.println("Employee is absent.");
+            workingHr = workingHr + hrs;
+            workingDays = workingDays + 1;
         }
+        int salary = workingHr * wagePerHr;
+        System.out.println("the salary of month is : " + salary);
     }
 }
 
@@ -44,7 +59,7 @@ public class EmpWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
-        utilityEmpWage empWage = new utilityEmpWage();
+        utilityEmpWage empWage = new utilityEmpWage();  //create object
         empWage.present();
     }
 
