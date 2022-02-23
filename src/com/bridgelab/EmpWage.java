bda2a1baa15d8ba9929834624277fd5fbbbd3716
nilofarmujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-02-23 16:23:30 +0530

    added switch case inplace of if statements

--- src/com/bridgelab/EmpWage.java
+++ src/com/bridgelab/EmpWage.java
@@ -1,49 +1,34 @@
 package com.bridgelab;
 
-import java.util.Random;
-
-//uc4
-
-/*
-      Program Using Switch case Employee is work
- */
-
-class utilityEmpWage {
-    Random rand = new Random();
-    public final int isPresent = 1;
-    public final int isAbsent = 2;
-    public final int isPartTime = 3;
-    public int wagePerHr = 20;
-    public int fullDayHr = 8;
-    public int partTimeHr = 4;
-    public int dailyWage;
-
-    public void present() {
-        int check = rand.nextInt(3) + 1;
-        switch (check) {
-            case isPresent : {
-                System.out.println("Employee is present.");
-                dailyWage = wagePerHr * fullDayHr;                  //condition 1
-                System.out.println("Daily wage is : " + dailyWage);
-            }
-
-            case isPartTime : {
-                System.out.println("Employee is present part time."); //condition 2
-                dailyWage = wagePerHr * partTimeHr;
-                System.out.println("Daily wage is : " + dailyWage);
-            }
-
-            case isAbsent : System.out.println("Employee is absent.");  //default statement
+public class EmpWage{
+    public static void main(String args[]) {
+        //constatnt
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int WAGE_PER_HR = 20;
+        int empType = (int) (Math.random() * 100) % 3;
+        int workingHours =0;
+        
+        /*
+        using switch case for employee present or absent.if employee present in full tm then working hr is 8,
+        if employee absent in part time then working hr is 4.
+         */
+        
+        switch (empType) {
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");  //condition 1
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Present Part time");  //condition 2
+                workingHours = 4;
+                break;
+                
+            default:
+                System.out.println("Employee is Absent");  // if case 1 and case 2 both are fail then default condition run
         }
+        //calculate daily wage
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
-}
-
-public class EmpWage {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation");
-        utilityEmpWage empWage = new utilityEmpWage(); //create object for utilityEmpWage class
-        empWage.present();
-    }
-
 }
